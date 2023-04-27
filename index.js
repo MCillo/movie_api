97960e11775047be29175817373d1bd8878756a0
MCillo <117599499+MCillo@users.noreply.github.com>
2023-04-27 16:06:15 -0400

    Update index.js
    updated line of code:
    app.use(bodyParser, json());
    to
    app.use(bodyParser.json());

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(bodyParser, json());
+app.use(bodyParser.json());
 app.use(methodOverride());
 
 app.use((err, req, res, next) => {
