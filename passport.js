be3e917c5c572f4577f94c63cde8255f06e5e657
MCillo <117599499+MCillo@users.noreply.github.com>
2023-06-03 18:52:07 -0400

    Update passport.js
    updated Passport (LocalStrategy) to work with .then, .catch due to callbacks no longer being supported

--- passport.js
+++ passport.js
@@ -11,20 +11,16 @@ passport.use(new LocalStrategy({
     usernameField: 'Username',
     passwordField: 'Password'
 }, (username, password, callback) => {
-    console.log(username + '  ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-            console.log(error);
-            return callback(error);
-        }
-
+    console.log(username + ' ' + password);
+    Users.findOne({ Username: username }).then((user) => {
         if (!user) {
-            console.log('incorrect username');
+            console.log('Incorrect username');
             return callback(null, false, { message: 'Incorrect username or password.' });
         }
-
-        console.log('finished');
+        console.log('Finished');
         return callback(null, user);
+    }).catch((error) => {
+        return callback(error)
     });
 }));
 
