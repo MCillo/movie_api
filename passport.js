8c3c92192a91d54606cd8b29d4a2670a55eac2ae
MCillo <117599499+MCillo@users.noreply.github.com>
2023-11-09 11:37:14 -0500

    Update passport.js
    added comments to code

--- passport.js
+++ passport.js
@@ -1,4 +1,12 @@
+/** 
+ *  passport is an authentication middleware for Node.js and Express
+ *  it will be used to authenticate users for the App
+ */
+
+// 
 const passport = require('passport'),
+
+
     LocalStrategy = require('passport-local').Strategy,
     Models = require('./models.js'),
     passportJWT = require('passport-jwt');
@@ -7,16 +15,23 @@ let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
     ExtractJWT = passportJWT.ExtractJwt;
 
+/**
+    * LocalStrategy takes the username and password from the http request body
+    * uses Mongoose to search the DataBase for a user with the same username
+    */
+
 passport.use(new LocalStrategy({
     usernameField: 'Username',
     passwordField: 'Password'
 }, (username, password, callback) => {
     console.log(username + ' ' + password);
     Users.findOne({ Username: username }).then((user) => {
+        // If the LocalStrategy does not match the username in the request body with a username in the database
         if (!user) {
             console.log('Incorrect username');
             return callback(null, false, { message: 'Incorrect username or password.' });
         }
+        // If the
         if (!user.validatePassword(password)) {
             console.log('Incorrect Password');
             return callback(null, false, { message: 'Incorrect Password.' });
